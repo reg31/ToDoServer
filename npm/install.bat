ac9e980a49f1127041cb0db1efb60bfc7bb2b652
reg <regis.04@free.fr>
2021-08-15 13:08:12 +0100

    update install script

--- npm/install.bat
+++ npm/install.bat
@@ -1,7 +1,7 @@
 cd %~dp0
 start npm install express --save
-start npm i @okta/jwt-verifier
-start npm i cors
-start npm i node-storage
-start npm i simple-encryptor
+start npm install @okta/jwt-verifier
+start npm install cors
+start npm install node-storage
+start npm install simple-encryptor --save
 pause
