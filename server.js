1d183f6a457e153cd7d9246a9818aae808a1a6ce
reg <regis.04@free.fr>
2021-08-29 13:05:04 +0100

    use port 5000 for Heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require("path");
 var cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
