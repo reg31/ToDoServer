9577464e0653fa6b9f4c39be442d61841272a50d
reg <regis.04@free.fr>
2021-08-29 13:24:13 +0100

    use environment port if any

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require("path");
 var cors = require('cors');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
